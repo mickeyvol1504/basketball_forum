c73c05247abd996d2654a4f556286fa75e6836fd
Michael Voloshyn <mickey15041989@gmail.com>
2015-07-26 16:07:45 +0300

    Add 'forum' to INSTALLED APS and add STATIC_URL and MEDIA_URL

--- worldbasket/worldbasket/settings.py
+++ worldbasket/worldbasket/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'forum'
+    'forum',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -105,3 +105,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+TEMPLATE_DIRS = (
+    'templates',
+    )
+
+SITE_NAME = 'Worldbasket'
+
